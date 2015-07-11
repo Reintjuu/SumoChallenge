328d5db4a782317d758ef1be558aacebbd6dcad4
Reinier de Vries <reiniervries@outlook.com>
2015-07-11 23:08:28 +0200

    Fixed a bug

--- Assets/Code/PlayerNetworkSetup.cs
+++ Assets/Code/PlayerNetworkSetup.cs
@@ -5,14 +5,12 @@ using UnityEngine.Networking;
 public class PlayerNetworkSetup : NetworkBehaviour
 {
 	[SerializeField] CharacterMovement mover;
-    [SerializeField] Camera follower;
 
 	void Start ()
     {
         if(isLocalPlayer)
         {
             mover.enabled = true;
-            follower.enabled = true;
         }
 	}
 }
