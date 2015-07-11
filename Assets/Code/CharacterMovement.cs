ed65a83a8da5137daf53ff4c76e7b2b225962a2e
Reinier de Vries <reiniervries@outlook.com>
2015-07-11 18:47:31 +0200

    Added basic multiplayer networking

--- Assets/Code/CharacterMovement.cs
+++ Assets/Code/CharacterMovement.cs
@@ -4,15 +4,12 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody2D))]
 public class CharacterMovement : MonoBehaviour
 {
-	[SerializeField]
-	private float speed = 2f;
-	[SerializeField]
-	private float jumpForce = 100f;
-	[SerializeField]
-	private float groundedTreshhold = .5f;
+	[SerializeField] private float speed = 2f;
+	[SerializeField] private float jumpForce = 100f;
+	[SerializeField] private float groundedTreshhold = .5f;
 
 	private float move = 0f;
-	private bool isGrounded = false;
+	private bool grounded = false;
 	private GameObject groundedOn = null;
 	private Rigidbody2D body;
 
@@ -21,20 +18,20 @@ public class CharacterMovement : MonoBehaviour
 		body = GetComponent<Rigidbody2D>();
 	}
 
-	void OnCollisionEnter2D(Collision2D collision)
+	void OnCollisionEnter2D(Collision2D collidee)
 	{
-		if(collision.gameObject.GetComponent<Rigidbody2D>().isKinematic)
-		{
-			foreach(ContactPoint2D contact in collision.contacts)
+		//if(collidee.gameObject.GetComponent<Rigidbody2D>().isKinematic)
+		//{
+			foreach(ContactPoint2D contact in collidee.contacts)
 			{
 				if (contact.normal.y > groundedTreshhold)
 				{
-					isGrounded = true;
-					groundedOn = collision.gameObject;
+					grounded = true;
+					groundedOn = collidee.gameObject;
 					break;
 				}
 			}
-		}
+		//}
 	}
 
 	void OnCollisionExit2D(Collision2D collision)
@@ -42,7 +39,7 @@ public class CharacterMovement : MonoBehaviour
 		if (collision.gameObject == groundedOn)
 		{
 			groundedOn = null;
-			isGrounded = false;
+			grounded = false;
 		}
 	}
 
@@ -51,7 +48,7 @@ public class CharacterMovement : MonoBehaviour
 		move = Input.GetAxis("Horizontal");
 		body.velocity = new Vector2(move * speed, body.velocity.y);
 
-		if (Input.GetKey(KeyCode.Space) && isGrounded)
+		if (Input.GetKey(KeyCode.Space) && grounded)
 		{
 			body.AddForce(new Vector2(body.velocity.x, jumpForce));
 		}
---
+++ Assets/Code/PlayerNetworkSetup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour
+{
+	[SerializeField] CharacterMovement mover;
+    [SerializeField] Camera follower;
+
+	void Start ()
+    {
+        if(isLocalPlayer)
+        {
+            mover.enabled = true;
+            follower.enabled = true;
+        }
+	}
+}
